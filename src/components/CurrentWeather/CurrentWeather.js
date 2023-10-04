3e4a5d2769d1cef5a50c55390883064313807d34
Tonimarie <tonimarie.morris@maxxpotential.com>
2023-10-04 12:56:50 -0400

    Refactor CurrentWeather to use css module

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,24 +1,24 @@
 /* displays all current weather-related data */
 import React from 'react'
-import './CurrentWeather.module.css'
-import WeatherContainer from '../WeatherContainer/WeatherContainer'
+import styles from './CurrentWeather.module.css'
+
 const CurrentWeather = ({ weather, conditions, icon, city, stateAbbr }) => {
     
     return(
         <>
-            {weather && <WeatherContainer>
-                {city && <header className='current-weather-header'><h2>{city}, {stateAbbr}</h2></header>}
-                <div className='current-weather-container'>
-                    <div className='current-weather-column'>
-                        <p className='current-temp'>{Math.round(weather.temp)} <span>&deg;</span></p>
-                        <p className='current-conditions'>{conditions}</p>
-                        <p className='current-feels-like'>Feels Like {Math.round(weather.feels_like)} <span>&deg;</span></p>
+            {weather && <div className={styles.currentWeatherCard}>
+                {city && <header className={styles.currentWeatherHeader}><h2>{city}, {stateAbbr}</h2></header>}
+                <div className={styles.currentWeatherContainer}>
+                    <div className={styles.currentWeatherColumn}>
+                        <p className={styles.currentTemp}>{Math.round(weather.temp)} <span>&deg;</span></p>
+                        <p className={styles.currentConditions}>{conditions}</p>
+                        <p className={styles.currentFeelsLike}>Feels Like {Math.round(weather.feels_like)} <span>&deg;</span></p>
                     </div>
-                    {conditions && <div className='weather-conditions-container'>
-                        {icon && <img className='weather-icon' alt='weather-icon' src={`http://openweathermap.org/img/wn/${icon}@4x.png`}/>}
+                    {conditions && <div className={styles.weatherConditionsContainer}>
+                        {icon && <img className={styles.weatherIcon} alt='weather-icon' src={`http://openweathermap.org/img/wn/${icon}@4x.png`}/>}
                     </div>}
                 </div>
-            </WeatherContainer>}
+            </div>}
         </>
     )
 }
