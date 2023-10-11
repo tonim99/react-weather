c178f1a9ad9cbce427f9a221ac2891ffed0fb632
Tonimarie <tonimarie.morris@maxxpotential.com>
2023-10-11 12:22:39 -0400

    Clean up comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+
+
 import React from 'react';
 import Weather from './api/Weather'
 
-import './App.css';
-
 function App() {
+
   return (
     <div className="App">
       <Weather />
     </div>
-  );
+    );
 }
 
-export default App;
+export default App;
