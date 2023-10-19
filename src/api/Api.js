0c6b4e4a27475e98701269504b74178ee340edfa
Tonimarie <tonimarie.morris@maxxpotential.com>
2023-10-19 10:01:59 -0400

    Add separate file for API calls

---
+++ src/api/Api.js
@@ -0,0 +1,9 @@
+    
+    
+    
+    const APIKey = process.env.REACT_APP_API_KEY;
+    const baseUrl = 'http://api.openweathermap.org';
+
+    export const fetchLatLon = (zip) =>  fetch(`${baseUrl}/geo/1.0/zip?zip=${zip}&appid=${APIKey}`)
+    export const fetchStateByLatLon = (lat, lon) => fetch(`${baseUrl}/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5&appid=${APIKey}`)
+    export const fetchWeather = (lat, lon) => (`${baseUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${APIKey}&units=imperial`)
