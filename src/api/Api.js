9dfb00d28a453a904efec0c548426239bfe946b4
Tonimarie <tonimarie.morris@maxxpotential.com>
2023-10-29 21:10:19 -0400

    Add error handling to remaining api calls

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,9 +1,45 @@
     
     
     
-    const APIKey = process.env.REACT_APP_API_KEY;
-    const baseUrl = 'https://api.openweathermap.org';
+const APIKey = process.env.REACT_APP_API_KEY;
+const baseUrl = 'https://api.openweathermap.org';
 
-    export const fetchLatLon = (zip) =>  fetch(`${baseUrl}/geo/1.0/zip?zip=${zip}&appid=${APIKey}`)
-    export const fetchStateByLatLon = (lat, lon) => fetch(`${baseUrl}/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5&appid=${APIKey}`)
-    export const fetchWeather = (lat, lon) => (`${baseUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${APIKey}&units=imperial`)
+export const getLatLon = (zip) =>  fetch(`${baseUrl}/geo/1.0/zip?zip=${zip}&appid=${APIKey}`)
+    .then(async (res) => {
+        if(!res.ok) {
+            const errorData = await res.json();
+            throw new Error(
+                errorData.message
+                    ? `Error Code: ${errorData.cod} - ${errorData.message}`
+                    : 'Invalid zip code. Please try again.'
+            );
+        }
+    return res.json()
+})  
+
+export const getStateByLatLon = (lat, lon) => fetch(`${baseUrl}/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5&appid=${APIKey}`)
+    .then(async (res) => {
+        if(!res.ok) {
+            const errorData = await res.json();
+            throw new Error(
+                errorData.message
+                    ? `Error Code: ${errorData.cod} - ${errorData.message}`
+                    : 'Something went wrong. Please refresh and try again.'
+            );
+        }
+    return res.json()
+})
+    
+    
+    export const getWeather = (lat, lon) => fetch(`${baseUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${APIKey}&units=imperial`)
+    .then(async (res) => {
+        if(!res.ok) {
+            const errorData = await res.json();
+            throw new Error(
+                errorData.message
+                    ? `Error Code: ${errorData.cod} - ${errorData.message}`
+                    : 'Something went wrong. Please refresh and try again.'
+            );
+        }
+    return res.json()
+    })
